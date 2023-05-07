6ea0fc40ad6660ebdaa66a1f72c166d584f64c7f
auxopen <114030887+auxopen@users.noreply.github.com>
2023-05-06 20:54:13 -0500

    Update abcdefghijk.lua

--- abcdefghijk.lua
+++ abcdefghijk.lua
@@ -2,7 +2,7 @@ getgenv().NfHFUhgfbU = newcclosure(function()
   return true
 end)
 
-getgenv().checkKey = newcclosure(function() 
+getgenv().MhfguURHhtI = newcclosure(function() 
   return true
 end)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/VersatileTeam/hydrogen-assets/main/scripts/user_interface_final.lua"))()
